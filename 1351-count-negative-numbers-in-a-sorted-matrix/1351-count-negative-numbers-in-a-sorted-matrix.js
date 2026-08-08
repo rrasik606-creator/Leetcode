606b6a473a86c9683dbab098dccc20a174d02d30
MUHAMMED RASHIK VP <rrasik606@gmail.com>
2026-08-08 09:47:46 +0530

    Time: 3 ms (20.73%), Space: 56.3 MB (20.73%) - LeetHub

---
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countNegatives = function(grid) {
+    let result=0;
+    for(array of grid){
+        for(item of array){
+            if(item<0){
+                result+=1;
+            }
+        }
+    }
+    return result
+};
