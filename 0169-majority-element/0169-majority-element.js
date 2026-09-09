23d7b346f6153d6d82a9df6597d72f26b09b2390
MUHAMMED RASHIK VP <rrasik606@gmail.com>
2026-09-09 09:50:16 +0530

    Time: 3 ms (63.71%), Space: 59.6 MB (10.87%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let candidate = 0;
+    let count = 0;
+
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+
+        if (num === candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return candidate;
+};
